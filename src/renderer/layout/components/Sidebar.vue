da83b9891dfbe90813b42017374d9e8aaad8181e
zq <836159458@qq.com>
2020-02-10 00:50:10 +0800

    滴答清单风格

--- src/renderer/layout/components/Sidebar.vue
+++ src/renderer/layout/components/Sidebar.vue
@@ -7,81 +7,75 @@
                 class="menu"
         >
             <a-menu-item key="all">全部记录</a-menu-item>
-
-            <a-menu-divider/>
-
-            <a-sub-menu key="sub1" @titleClick="">
-                        <span slot="title">
-                            <a-icon type="tags"/>
-                            <span>标签</span>
-                        </span>
-                <a-menu-item key="5">Option 5</a-menu-item>
-                <a-menu-item key="6">Option 6</a-menu-item>
-                <a-sub-menu key="sub3" title="Submenu">
-                    <a-menu-item key="7">Option 7</a-menu-item>
-                    <a-menu-item key="8">Option 8</a-menu-item>
-                </a-sub-menu>
-            </a-sub-menu>
         </a-menu>
 
-        <a-menu
-                mode="inline"
-                class="menu"
-                :defaultOpenKeys="['sub1', 'sub2', 'sub3']"
+
+        <a-tabs
+                defaultActiveKey="groups"
+                @change=""
+                style="text-align: center"
         >
-            <a-sub-menu key="sub1" @titleClick="">
+            <a-tab-pane tab="分组" key="groups" style="text-align: left">
+                <a-menu
+                        mode="inline"
+                        class="menu"
+                        :defaultOpenKeys="['group1', 'group2', 'group3']"
+                >
+                    <a-sub-menu key="group1">
                         <span slot="title">
                             <a-icon type="folder"/>
-                            <span>数据库1</span>
+                            <span>分组1</span>
                         </span>
-                <a-menu-item key="5">Option 5</a-menu-item>
-                <a-menu-item key="6">Option 6</a-menu-item>
-                <a-sub-menu key="sub3" title="Submenu">
-                    <a-menu-item key="7">Option 7</a-menu-item>
-                    <a-menu-item key="8">Option 8</a-menu-item>
-                </a-sub-menu>
-            </a-sub-menu>
-            <a-sub-menu key="sub2">
-                        <span slot="title">
-                            <a-icon type="setting"/>
-                            <span>数据库2</span>
-                        </span>
-                <a-menu-item key="9">Option 9</a-menu-item>
-                <a-menu-item key="10">Option 10</a-menu-item>
-                <a-menu-item key="11">Option 11</a-menu-item>
-                <a-menu-item key="12">Option 12</a-menu-item>
-            </a-sub-menu>
+                        <a-menu-item key="group1-1">
+                            <a-icon type="folder"/>
+                            <span>分组1-1</span>
+                        </a-menu-item>
+                        <a-menu-item key="group1-2">
+                            <a-icon type="folder"/>
+                            <span>分组1-2</span>
+                        </a-menu-item>
+                        <a-sub-menu key="sub3" title="Submenu">
+                            <a-menu-item key="7">Option 7</a-menu-item>
+                            <a-menu-item key="8">Option 8</a-menu-item>
+                        </a-sub-menu>
+                    </a-sub-menu>
 
-            <a-sub-menu key="sub3">
-                        <span slot="title">
-                            <a-icon type="setting"/>
-                            <span>数据库3</span>
-                        </span>
-                <a-menu-item key="9">Option 9</a-menu-item>
-                <a-menu-item key="10">Option 10</a-menu-item>
-                <a-menu-item key="11">Option 11</a-menu-item>
-                <a-menu-item key="12">Option 12</a-menu-item>
-            </a-sub-menu>
+                    <a-menu-item key="add-group">
+                        <a-icon type="plus-circle" />
+                        <span>添加分组</span>
+                    </a-menu-item>
 
-            <a-menu-divider/>
+                    <a-menu-divider/>
 
-            <a-menu-item key="trash">回收站</a-menu-item>
-        </a-menu>
+                    <a-menu-item key="deleted">
+                        <a-icon type="delete" />
+                        <span>回收站</span>
+                    </a-menu-item>
+
+                    <a-menu-divider/>
+                </a-menu>
+            </a-tab-pane>
+            <a-tab-pane tab="标签" key="tabs">
+
+            </a-tab-pane>
+        </a-tabs>
     </div>
 </template>
 
 <script>
   import Vue from 'vue'
-  import {Layout, Menu} from 'ant-design-vue'
+  import {Layout, Menu, Tabs} from 'ant-design-vue'
 
   Vue.use(Layout)
   Vue.use(Menu)
+  Vue.use(Tabs)
 
   export default {
     name: 'NSidebar',
     comments: {
       Layout,
-      Menu
+      Menu,
+      Tabs
     }
   }
 </script>
